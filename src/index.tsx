519e373ae7553f875e3f4fd7124446158891fdd6
sh0rk1n <sh0rk1n@mail.ru>
2024-07-06 20:53:04 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app/App';
+import './app/styles/index.scss';
+
+const container = document.getElementById('root');
+
+if (!container) {
+    throw new Error(
+        'Контейнер root не найден. НЕ удалось вмонтировать реакт приложение',
+    );
+}
+
+const root = createRoot(container);
+
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+);
